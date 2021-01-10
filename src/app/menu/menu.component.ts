5064b7d23ee4ecea3682d28cce330a4d9de47eb0
Danil Beltyukov <beltyukovdanil@gmail.com>
2021-01-10 15:42:03 +0300

    scroller init

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
   open = false;
+  scrollerVisible = false;
+
+  ngOnInit() {
+    window.onscroll = () => {
+      if (window.scrollY > 20) {
+        this.scrollerVisible = true;
+      }
+    };
+  }
 
   onMenuButtonClick() {
     this.open = !this.open;
   }
 
+  onHomeButtonClick() {
+    if (window.scrollY !== 0) {
+      window.scroll({ top: 0, behavior: 'smooth' });
+    }
+  }
 }
