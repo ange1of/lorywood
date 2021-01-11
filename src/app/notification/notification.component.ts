19de7a125031b0fb5ac37b7e2cf21bf87bdfc1a8
Danil Beltyukov <beltyukovdanil@gmail.com>
2021-01-11 19:25:31 +0300

    fix always show

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -20,12 +20,12 @@ export class NotificationComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.visible = true;
-    console.log(this.remember);
     if (this.remember) {
       if (!window.localStorage.getItem(`${this.remember}:showed`)) {
         this.visible = true;
       }
+    } else {
+      this.visible = true;
     }
   }
 
